e7804d4b8aebac70fb887bc46a56a2bf22944149
Mephilos <sth4628@hotmail.com>
2025-11-25 13:41:12 +0900

    소수 채 복습

---
+++ 251125/1929.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<bool> isPrime(1000001, true);
+
+void FindPriem()
+{
+    isPrime[0] = false;
+    isPrime[1] = false;
+
+    for (int i = 2; i * i <= 1000000; i++)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= 1000000; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    FindPriem();
+    int N, M;
+
+    cin >> N >> M;
+
+    for (int i = N; i <= M; i++)
+    {
+        if (isPrime[i])
+            cout << i << '\n';
+    }
+}
