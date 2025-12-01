a1ecdbb51ed924ca4b331a45316b227b54f81a00
Mephilos <sth4628@hotmail.com>
2025-12-01 09:22:59 +0900

    업데이트

---
+++ 251129/1005.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+#define endl '\n'
+
+vector<vector<int>> buildOrder;
+vector<int> indgree;
+vector<int> totalTime;
+vector<int> buildTime;
